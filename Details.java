ad02f8e7c25c3d9828241f28c806110c9640523e
Md. Asif Karim Chayon <79499779+9yon6yon-6yon@users.noreply.github.com>
2021-09-24 20:55:58 +0600

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Details {
+     String name;
+        String phone;
+        String email;
+        String address;
+    
+    Details(String name,String phone,String email,String address){
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+    
+}
