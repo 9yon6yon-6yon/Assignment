86ed9b35d8cd6bd489c16329472a74de0fad3912
Md. Asif Karim Chayon <79499779+9yon6yon-6yon@users.noreply.github.com>
2021-09-26 18:36:15 +0600

    JAVA GUI exam

---
+++ OOPLabFinalexamination/Info.java
@@ -0,0 +1,12 @@
+package OOPLabFinalexamination;
+public class Info {
+    String name;
+    String id;
+    String marks;
+
+    Info(String Name, String ID, String Marks) {
+        this.name = Name;
+        this.id = ID;
+        this.marks = Marks;
+    }
+}
